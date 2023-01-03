5fcb7da575b7155071f21d9661662e5805346e59
kit <kit@cubetiqs.com>
2023-01-03 11:39:38 +0700

    social media button

---
+++ lib/components/social_media_button/social_media_button.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: file_names
+
+import 'package:component_sdk/components/text/custom_text_component.dart';
+import 'package:flutter/material.dart';
+
+class SocialMediaButton extends StatelessWidget {
+  final String title;
+  final Color? borderColor;
+  final String image;
+  final String? fallbackText;
+  final Color? iconColor;
+  final Color? fontColor;
+  final Color? buttonColor;
+  const SocialMediaButton(
+      {Key? key,
+      this.fallbackText,
+      this.borderColor,
+      this.title = "",
+      this.image = "",
+      this.iconColor,
+      this.fontColor,
+      this.buttonColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 45,
+      decoration: BoxDecoration(
+        color: buttonColor ?? Colors.white,
+        border: Border.all(width: 1, color: borderColor ?? Colors.transparent),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // ignore: avoid_unnecessary_containers
+          Container(
+            child: Image.asset(
+              image,
+              height: 20,
+              color: iconColor,
+            ),
+          ),
+          const SizedBox(width: 10),
+          CTextCustoms(
+            min: 12,
+            t: true,
+            text: title,
+            fallbackText: fallbackText,
+            style: TextStyle(color: fontColor ?? Colors.black),
+          ),
+        ],
+      ),
+    );
+  }
+}
