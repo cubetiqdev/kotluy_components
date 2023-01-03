37341ce0884fdbd04e22f514bf61a92ac872f62c
kit <kit@cubetiqs.com>
2023-01-03 15:46:34 +0700

    update social_media_button

--- lib/components/social_media_button/social_media_button.dart
+++ lib/components/social_media_button/social_media_button.dart
@@ -12,6 +12,11 @@ class SocialMediaButton extends StatelessWidget {
   final Color? fontColor;
   final Color? buttonColor;
   final bool t;
+  final void Function()? onTap;
+  final double? height;
+  final double? width;
+  final double? imageHeight;
+  final double? iamgeWidth;
   const SocialMediaButton(
       {Key? key,
       this.fallbackText,
@@ -21,39 +26,49 @@ class SocialMediaButton extends StatelessWidget {
       this.iconColor,
       this.fontColor,
       this.buttonColor,
-      this.t = false})
+      this.t = false,
+      this.onTap,
+      this.height,
+      this.width,
+      this.imageHeight,
+      this.iamgeWidth})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 45,
-      decoration: BoxDecoration(
-        color: buttonColor ?? Colors.white,
-        border: Border.all(width: 1, color: borderColor ?? Colors.transparent),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // ignore: avoid_unnecessary_containers
-          Container(
-            child: Image.asset(
-              image,
-              height: 20,
-              color: iconColor,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width ?? double.infinity,
+        height: height ?? 45,
+        decoration: BoxDecoration(
+          color: buttonColor ?? Colors.white,
+          border:
+              Border.all(width: 1, color: borderColor ?? Colors.transparent),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // ignore: avoid_unnecessary_containers
+            Container(
+              child: Image.asset(
+                image,
+                width: iamgeWidth,
+                height: imageHeight ?? 20,
+                color: iconColor,
+              ),
             ),
-          ),
-          const SizedBox(width: 10),
-          CTextCustoms(
-            min: 12,
-            t: t,
-            text: title,
-            fallbackText: fallbackText,
-            style: TextStyle(color: fontColor ?? Colors.black),
-          ),
-        ],
+            const SizedBox(width: 10),
+            CTextCustoms(
+              min: 12,
+              t: t,
+              text: title,
+              fallbackText: fallbackText,
+              style: TextStyle(color: fontColor ?? Colors.black),
+            ),
+          ],
+        ),
       ),
     );
   }
