5a35be87c1000affc13be410e94947731c099865
kit <kit@cubetiqs.com>
2023-01-17 14:00:22 +0700

    add button choose language

---
+++ lib/components/language_component/button_choose_language.dart
@@ -0,0 +1,68 @@
+import 'package:component_sdk/constant/colors/color.dart';
+import 'package:component_sdk/constant/fonts/default_font.dart';
+import 'package:flutter/material.dart';
+
+class ButtonChooseLanguage extends StatelessWidget {
+  final String? title;
+  final String? image;
+  final String? icon;
+  final Color? colorSelect;
+  final Color? colorText;
+  final Color? colorICon;
+  final Color? borderColor;
+  final GestureTapCallback? onTap;
+  const ButtonChooseLanguage(
+      {super.key,
+      this.icon,
+      this.image,
+      this.title,
+      this.onTap,
+      this.colorSelect,
+      this.colorText,
+      this.colorICon,
+      this.borderColor});
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      radius: 12,
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        height: 44,
+        padding: const EdgeInsets.only(
+            left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
+        // ignore: prefer_const_constructors
+        alignment: Alignment(0, 0),
+        decoration: BoxDecoration(
+          color: colorSelect,
+          border:
+              Border.all(width: 1.0, color: borderColor ?? ThemeColor.DARK_D4),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisSize: MainAxisSize.max,
+          children: [
+            Image.asset(
+              image!,
+              width: 31.0,
+              height: 20.0,
+            ),
+            Text(
+              title!,
+              style: KLabelTextMedium14.copyWith(
+                  color: colorText ?? ThemeColor.LIGHT_WHITE),
+            ),
+            Image.asset(
+              icon!,
+              width: 31.0,
+              height: 20.0,
+              color: colorICon ?? ThemeColor.DARK_D4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
