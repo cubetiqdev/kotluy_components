a51a4e923ca2d2fc60eacb7f7a31d6698b3599f5
kit <kit@cubetiqs.com>
2023-02-06 11:52:40 +0700

    update

--- lib/graphQL/service/graphql_query(kit).dart
+++ lib/graphQL/service/graphql_query(kit).dart
@@ -61,7 +61,7 @@ class GraphQLQuery {
     }
   }
 
-  Future mutationDataWithNoAuth({required String mutation}) async {
+  Future<QueryResult> mutationDataWithNoAuth({required String mutation}) async {
     HttpLink httpLink = HttpLink(
       url,
     );
