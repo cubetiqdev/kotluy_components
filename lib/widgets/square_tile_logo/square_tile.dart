9a5e6ee7ff686806061a333624796bc0e3bec15d
kit <kit@cubetiqs.com>
2023-02-13 11:55:28 +0700

    update ontap

--- lib/widgets/square_tile_logo/square_tile.dart
+++ lib/widgets/square_tile_logo/square_tile.dart
@@ -13,7 +13,7 @@ class SquareTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
+      onTap: onTap ?? () {},
       child: Container(
         padding: const EdgeInsets.all(20),
         decoration: BoxDecoration(
