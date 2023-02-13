9b56ab24ac8ccb2e35d2504e09789bd694231b24
kit <kit@cubetiqs.com>
2023-02-13 11:46:34 +0700

    add square tile

---
+++ lib/widgets/square_tile_logo/square_tile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class SquareTile extends StatelessWidget {
+  final String imagePath;
+  final void Function()? onTap;
+  const SquareTile({
+    super.key,
+    required this.imagePath,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey.shade600,
+          ),
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.grey.shade200,
+        ),
+        child: Image.asset(
+          imagePath,
+          height: 3.h,
+        ),
+      ),
+    );
+  }
+}
