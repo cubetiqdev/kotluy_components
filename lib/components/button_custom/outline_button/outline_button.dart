c84954edfbbdf9b5e8d46bdc3ecebc2edfd6b2b1
kit <kit.chea168@gmail.com>
2023-04-06 09:08:56 +0700

    update

--- lib/components/button_custom/outline_button/outline_button.dart
+++ lib/components/button_custom/outline_button/outline_button.dart
@@ -9,8 +9,6 @@ class OutlineButton extends StatelessWidget {
   final Color? colorText;
   final Color? colorIcon;
   final Color? borderColor;
-  final double padHorizontal;
-  final double padVertical;
   final double? height;
   final double? borderRadius;
   final double? iconSize;
@@ -24,8 +22,6 @@ class OutlineButton extends StatelessWidget {
     this.colorText,
     this.colorIcon,
     this.borderColor,
-    this.padHorizontal = 20,
-    this.padVertical = 0,
     this.height,
     this.borderRadius,
     this.iconSize,
@@ -34,38 +30,33 @@ class OutlineButton extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-          horizontal: padHorizontal, vertical: padVertical),
-      child: InkWell(
-        onTap: onTap,
-        borderRadius: BorderRadius.circular(borderRadius ?? 50),
-        child: Container(
-          height: height,
-          padding: contentPadding ??
-              const EdgeInsets.only(
-                  left: 35.0, right: 30.0, top: 15, bottom: 15),
-          decoration: BoxDecoration(
-            color: fillColor,
-            border: Border.all(
-                width: 1.0, color: borderColor ?? ThemeColor.DARK_D4),
-            borderRadius: BorderRadius.circular(borderRadius ?? 50),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                title,
-                style: KLabelTextMedium14.copyWith(
-                    color: colorText ?? ThemeColor.DARK_Black),
-              ),
-              Icon(
-                icon ?? Icons.arrow_forward_ios_rounded,
-                color: colorIcon ?? ThemeColor.DARK_D4,
-                size: 20,
-              ),
-            ],
-          ),
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(borderRadius ?? 50),
+      child: Container(
+        height: height,
+        padding: contentPadding ??
+            const EdgeInsets.only(left: 35.0, right: 30.0, top: 15, bottom: 15),
+        decoration: BoxDecoration(
+          color: fillColor,
+          border:
+              Border.all(width: 1.0, color: borderColor ?? ThemeColor.DARK_D4),
+          borderRadius: BorderRadius.circular(borderRadius ?? 50),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: KLabelTextMedium14.copyWith(
+                  color: colorText ?? ThemeColor.DARK_Black),
+            ),
+            Icon(
+              icon ?? Icons.arrow_forward_ios_rounded,
+              color: colorIcon ?? ThemeColor.DARK_D4,
+              size: 20,
+            ),
+          ],
         ),
       ),
     );
