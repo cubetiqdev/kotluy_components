18286a02789955200be9424f689ec5066d5bcefc
kit <kit.chea168@gmail.com>
2023-07-04 13:49:37 +0700

    update

--- lib/console/console.dart
+++ lib/console/console.dart
@@ -7,50 +7,68 @@ class Console {
   static bool _isDebug = kDebugMode;
 
   static void log(text, data) {
-    if (!_isDebug) return;
-    if (text == null || text == '') {
-      text = 'Console.log';
+    if (_isDebug == false) {
+      return;
+    } else {
+      if (text == null || text == '') {
+        text = 'Console.log';
+      }
+
+      Print.out('\x1B[32m================== $text ==================\x1B[0m');
+      Print.out(data);
+      Print.out('\x1B[32m===========================================\x1B[0m');
     }
-    Print.out('\x1B[32m================== $text ==================\x1B[0m');
-    Print.out(data);
-    Print.out('\x1B[32m===========================================\x1B[0m');
   }
 
   static void print(text) {
-    if (!_isDebug) return;
-    Print.out(text);
+    if (_isDebug == false) {
+      return;
+    } else {
+      Print.out(text);
+    }
   }
 
   static void log2({
     String? name,
     dynamic data,
   }) {
-    if (!_isDebug) return;
-    log(name, data);
+    if (_isDebug == false) {
+      return;
+    } else {
+      log(name, data);
+    }
   }
 
   static void warning(text, data) {
-    if (!_isDebug) return;
-    Print.out('\x1B[33m================== $text ==================\x1B[0m');
-    Print.out(data);
-    Print.out('\x1B[33m===========================================\x1B[0m');
+    if (_isDebug == false) {
+      return;
+    } else {
+      Print.out('\x1B[33m================== $text ==================\x1B[0m');
+      Print.out(data);
+      Print.out('\x1B[33m===========================================\x1B[0m');
+    }
   }
 
   static void error(text, data) {
-    if (!_isDebug) return;
-
-    Print.out('\x1B[31m================== $text ==================\x1B[0m');
-    Print.out(data);
-    Print.out('\x1B[31m===========================================\x1B[0m');
+    if (_isDebug == false) {
+      return;
+    } else {
+      Print.out('\x1B[31m================== $text ==================\x1B[0m');
+      Print.out(data);
+      Print.out('\x1B[31m===========================================\x1B[0m');
+    }
   }
 
   static void jsonAll(data) {
-    if (!_isDebug) return;
-    JsonEncoder encoder = new JsonEncoder.withIndent('  ');
-    String prettylint = encoder.convert(data);
-    prettylint
-        .split('\n')
-        .forEach((element) => Console.log("Render JSON", prettylint));
+    if (_isDebug == false) {
+      return;
+    } else {
+      JsonEncoder encoder = new JsonEncoder.withIndent('  ');
+      String prettylint = encoder.convert(data);
+      prettylint
+          .split('\n')
+          .forEach((element) => Console.log("Render JSON", prettylint));
+    }
   }
 }
 
